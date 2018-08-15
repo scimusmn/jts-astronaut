86656c5dba8a5d725f5f0a8cf281c349c2faea27
Aaron Heidgerken-Greene <aheidgerken@smm.org>
2018-08-15 08:40:31 -0500

    fixing windows problems

--- ForBoot/appData/config.js
+++ ForBoot/appData/config.js
@@ -17,22 +17,22 @@ module.exports = {
       //displayId: '724059286',
       //url: 'http://localhost/index.html' //use for web protocol
       file: 'local/name/index.html',
-      size: { width: 192, height: 108 },
-      position: { x: 0, y: 200 },
+      //size: { width: 192, height: 108 },
+      //position: { x: 0, y: 200 },
     },
     {
       label: 'name_2',
       //displayId: '724059286',
       file: 'local/name/index.html',
-      size: { width: 192, height: 108 },
-      position: { x: 192, y: 200 },
+      //size: { width: 192, height: 108 },
+      //position: { x: 192, y: 200 },
     },
     {
       label: 'name_3',
       //displayId: '724059286',
       file: 'local/name/index.html',
-      size: { width: 192, height: 108 },
-      position: { x: 384, y: 200 },
+      //size: { width: 192, height: 108 },
+      //position: { x: 384, y: 200 },
     },
   ],
 };
